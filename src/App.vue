0931ea80c63a1036c873c1dd8a01a4645ae55746
Bundara123 <bundarareuk@gmail.com>
2023-09-21 10:48:23 +0700

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Tags from './components/Tags.vue';
-const tagAtive = 1;
-const tags = [];
-for (let i = 0; i < 200; i++) {
-  tags.push({
-    id: i,
-    title: `popular ${i}`,
-  });
-}
-</script>
-
 <template>
-  <div class="overflow-x-scroll w-full">
+  <div><SwiperSlider /></div>
+  <!-- <div class="overflow-x-scroll w-full">
     <div class="h-[35px] flex flex-row gap-1.5 px-2.5">
       <Tags
         v-for="(tag, index) in tags"
@@ -24,29 +11,20 @@ for (let i = 0; i < 200; i++) {
         :isPopular="1"
       />
     </div>
-  </div>
-  <div
-    class="
-      flex flex-row
-      overflow-x-scroll
-      space-x-24
-      bg-purple-700
-      p-4
-      mx-14
-      mt-5
-      h-36
-    "
-  >
-    <div class="bg-yellow-400 p-2 m-2">1</div>
-    <div class="bg-yellow-400 p-2 m-2">2</div>
-    <div class="bg-yellow-400 p-2 m-2">3</div>
-    <div class="bg-yellow-400 p-2 m-2">4</div>
-    <div class="bg-yellow-400 p-2 m-2">5</div>
-    <div class="bg-yellow-400 p-2 m-2">6</div>
-  </div>
-  <HelloWorld color="#245682" />
+  </div> -->
 </template>
-
+<script setup lang="ts">
+import Tags from './components/Tags.vue';
+import SwiperSlider from './components/SwiperSlider.vue';
+const tagAtive = 1;
+const tags = [];
+for (let i = 0; i < 200; i++) {
+  tags.push({
+    id: i,
+    title: `popular ${i}`,
+  });
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
